1ae89d509a35c18f92ad0094a22b87a37039b35a
Alfex-1 <114829547+Alfex-1@users.noreply.github.com>
2024-08-29 11:09:19 +0200

    update

--- Nutri-score/src/tools/Développement modèles/1. Choix_meilleur_modèle.py
+++ Nutri-score/src/tools/Développement modèles/1. Choix_meilleur_modèle.py
@@ -6,9 +6,10 @@ np.random.seed(42)
 df1 = pd.read_csv(r"C:\Donnees_nutriscore_v3\5Data_no_miss_unbalanced.csv", sep=',')
 df2 = pd.read_csv(r"C:\Donnees_nutriscore_v3\5Data_no_miss_balanced.csv", sep=',')
 
+
 # Encodage de la variable cible : 1 à 4
-df1, infos1 = encoding_all_data(df, reverse=True)
-df2, infos2 = encoding_all_data(df, reverse=True)
+df1, infos1 = encoding_all_data(df1, reverse=True)
+df2, infos2 = encoding_all_data(df2, reverse=True)
 
 # Division des donnees de la base non-equilibree
 X1 = df1.drop('NutriScore', axis=1)  # Variables predictives
@@ -29,117 +30,19 @@ xg, ada, cat, lg = XGBClassifier(random_state=42), AdaBoostClassifier(random_sta
 
 # Chercher les modeles optimaux sur la base non-EQUILIBREE
 
-# XGBoost
-xg_non_eq = xgboost_models(model=xg,
-               nb_estimators=np.arange(10,70,5),
-               learn_rate = np.arange(0.1,1.1,0.1),
-               l1 = np.arange(0,2.2,0.2),
-               l2 = np.arange(0,2.2,0.2),
-               gamma = np.arange(0,2.2,0.2),
-               max_depth = np.arange (2,12,2),
-               metric='f1_score',
-               average='weighted',
-               selected_models=3,
-               X_train=X_train1, y_train=y_train1, X_test=X_test1, y_test=y_test1)
+# Initialiser le modèle
+mlp = MLPClassifier(solver='adam',max_iter = 1000)
 
-# Adaboost
+# Chercher le MLP optimal sur leux bases non-EQUILIBREE
 
-ada_non_eq = adaboost_models(model=ada,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    max_depth_RF=np.arange (2,12,2),
-    metric='f1_score',
-    verage="weighted",
-    selected_models=3,
-    X_train=X_train1, y_train=y_train1, X_test=X_test1, y_test=y_test1)
+hidden_layer_sizes = generate_layer_combinations(max_layers=4, max_neurons=4)
+activation = ['tanh', 'relu']
+alpha = np.arange(0.1,1.1,0.1)
+learning_rate = np.arange(0.1, 1.1, 0.1)
 
-# Catboost
+# Pour la base equilibree
+results_df_equ = CV_parameters_classif(mlp, hidden_layer_sizes, activation, alpha, learning_rate, selected_model=5,average='macro',X_train=X_train2,X_test=X_test2,y_train=y_train2,y_test=y_test2)
 
-cat_non_eq = catboost_models(
-    model=model,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    l2=np.arange(0,2.2,0.2),
-    max_depth = np.arange (2,12,2),
-    metric='f1_score',
-    average="weighted",
-    selected_models=3,
-    X_train=X_train1, y_train=y_train1, X_test=X_test1, y_test=y_test1)
+# Pour la base non-equilibree
+results_df_non_eq = CV_parameters_classif(mlp, hidden_layer_sizes, activation, alpha, learning_rate, selected_model=5,average='weighted',X_train=X_train1,X_test=X_test1,y_train=y_train1,y_test=y_test1)
 
-# LightGBM
-
-lgb_non_eq = lightgbm_models(
-    model = model,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    l1=np.arange(0,2.2,0.2),
-    l2=np.arange(0,2.2,0.2),
-    max_depth=np.arange (2,12,2),
-    metric='f1_score',
-    average='weighted',
-    selected_models=3,
-    X_train=X_train1, y_train=y_train1, X_test=X_test1, y_test=y_test1)
-
-# Chercher les modeles optimaux sur la base EQUILIBRE
-
-# XGBoost
-xg_eq = xgboost_models(model=xg,
-               nb_estimators=np.arange(10,70,5),
-               learn_rate = np.arange(0.1,1.1,0.1),
-               l1 = np.arange(0.2,1.2,0.2),
-               l2 = np.arange(0,1.2,0.2),
-               gamma = np.arange(0,1.2,0.2),
-               max_depth = np.arange (2,12,2),
-               metric='precision',
-               average='macro',
-               selected_models=3,
-               X_train=X_train2, y_train=y_train2, X_test=X_test2, y_test=y_test2)
-
-# Adaboost
-
-ada_eq = adaboost_models(model=ada,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    max_depth_RF=np.arange (2,12,2),
-    metric='precision',
-    verage="macro",
-    selected_models=3,
-    X_train=X_train2, y_train=y_train2, X_test=X_test2, y_test=y_test2)
-
-# Catboost
-
-cat_eq = catboost_models(
-    model=model,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    l2=np.arange(0,2.2,0.2),
-    max_depth = np.arange (2,12,2),
-    metric='precision',
-    average="macro",
-    selected_models=3,
-    X_train=X_train2, y_train=y_train2, X_test=X_test2, y_test=y_test2)
-
-# LightGBM
-
-lgb_eq = lightgbm_models(
-    model = model,
-    nb_estimators=np.arange(10,70,5),
-    learn_rate=np.arange(0.1,1.1,0.1),
-    l1=np.arange(0,2.2,0.2),
-    l2=np.arange(0,2.2,0.2),
-    max_depth=np.arange (2,12,2),
-    metric='precision',
-    average='macro',
-    selected_models=3,
-    X_train=X_train2, y_train=y_train2, X_test=X_test2, y_test=y_test2)
-
-# Developpement du modele optimal
-
-eval_model,model_opti = model_opti(model="xg",
-                                   n_estimators=0,
-                                   learning_rate=0,
-                                   max_depth=0,
-                                   l1=0,
-                                   l2=0,
-                                   gamma=0,
-                                   average="weighted")
